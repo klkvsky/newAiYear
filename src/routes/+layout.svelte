5eef8114ade6a35b19c025ef30f402dabc8998f6
Kulikovsky <80684329+klkvsky@users.noreply.github.com>
2022-12-27 03:47:13 +0500

    v0.5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,31 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	//
+	function randomIntFromInterval(min: number, max: number) {
+		// min and max included
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
+	let rndInt: number = randomIntFromInterval(0, 391322);
+
+	function updateRndInt() {
+		rndInt = randomIntFromInterval(0, 391322);
+
+		setTimeout(updateRndInt, 10000);
+	}
+
+	updateRndInt();
 </script>
 
 <main
-	class="w-screen h-screen dark:bg-black dark:text-white font-mono px-4 flex flex-col items-center gap-[30px] pt-[10vh]"
+	class="max-w-screen min-h-[90vh] max-h-fit bg-black text-white font-mono px-4 py-[10vh] overflow-hidden"
 >
 	<slot />
+	<!-- {#key rndInt}
+		<img
+			src={`https://archillect.mhsattarian.workers.dev/${rndInt}/img`}
+			alt=""
+			class="fixed top-[2.5vh] left-[2vw] z-0 opacity-20 h-[85vh] w-[96vw] object-cover"
+		/>
+	{/key} -->
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { Configuration, OpenAIApi } from 'openai';
 	//
 	const configuration = new Configuration({
-		apiKey: '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'
+		apiKey: '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'
 	});
 	const openai = new OpenAIApi(configuration);
 	//
@@ -32,23 +32,80 @@
 		apiRequest(animalInput);
 		animalInput = '';
 	}
+
+	//
+
+	function randomIntFromInterval(min, max) {
+		// min and max included
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
+
+	let rndInt = randomIntFromInterval(0, 391322);
+
+	function updateRndInt() {
+		rndInt = randomIntFromInterval(0, 391322);
+
+		setTimeout(updateRndInt, 10000);
+	}
+
+	updateRndInt();
 </script>
 
-<h1 class="font-bold text-4xl text-center">New Year Resolutions Ai Edition</h1>
-<input
-	type="text"
-	class="bg-transparent border border-black dark:border-white rounded-full px-[30px] py-[15px] w-full font-medium text-lg"
-	placeholder="От кого пожелание?"
-/>
-<button
-	class="border border-black dark:border-white rounded-full px-[30px] py-[15px] w-full font-medium text-lg"
-	on:click={() => {
-		onSubmit();
-	}}
->
-	Узнать
-</button>
+<div class="flex flex-col items-center gap-[30px] relative z-10">
+	<div class="grid grid-cols-3 -mb-7">
+		<div
+			class="w-[28vw] bg-white/30 backdrop-blur-md relative z-10 bg-clip-content aspect-square rounded-full object-cover"
+		/>
+		<h1 class="font-bold text-4xl text-right relative z-10 col-span-2 -translate-y-4">
+			New AI Year Resolution
+		</h1>
+	</div>
 
-{#if result}
-	<p class="text-center">{result}</p>
-{/if}
+	<div class="flex flex-row items-center gap-[10px] w-full ">
+		<!-- <img
+			src={`https://archillect.mhsattarian.workers.dev/${rndInt}/img`}
+			alt=""
+			class="w-screen h-screen aspect-square fixed top-0 left-0 z-0 rounded-full object-cover opacity-50 rounded-b-none"
+		/> -->
+		<img
+			src={`https://archillect.mhsattarian.workers.dev/${rndInt}/img`}
+			alt=""
+			class="w-screen h-screen aspect-square fixed top-0 left-0 z-0 rounded-full object-cover opacity-20 rounded-b-none"
+		/>
+		<div
+			class="w-2/6 bg-white/20 backdrop-blur-md relative z-10 bg-clip-content aspect-square rounded-full object-cover animate-pulse"
+		/>
+		<div
+			class="w-2/6 bg-white/10 backdrop-blur-md relative z-10 bg-clip-content aspect-square rounded-full object-cover"
+		/>
+		<div
+			class="w-2/6 bg-white/30 backdrop-blur-md relative z-10 bg-clip-content aspect-square rounded-full object-cover"
+		/>
+		<!-- <img
+			src={`https://archillect.mhsattarian.workers.dev/${rndInt}/img`}
+			alt=""
+			class="w-2/6 aspect-square rounded-full object-cover"
+		/>
+		<img
+			src={`https://archillect.mhsattarian.workers.dev/${rndInt}/img`}
+			alt=""
+			class="w-2/6 aspect-square rounded-full object-cover"
+		/> -->
+	</div>
+	<input
+		type="text"
+		class="bg-white/10 backdrop-blur-md  rounded-full px-[30px] py-[15px] w-full font-medium text-lg relative z-10 placeholder:text-white"
+		placeholder="От кого пожелание?"
+	/>
+	<button
+		class=" rounded-full px-[30px] py-[15px] w-full font-medium text-lg relative z-10 bg-white/10 backdrop-blur-md"
+		on:click={() => {
+			onSubmit();
+		}}
+	>
+		Узнать
+	</button>
+	{#if result}
+		<p class="text-center relative z-10">{result}</p>
+	{/if}
+</div>
